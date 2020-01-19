906a23ed8b812464b67f70613c08ca4be75ea129
Mirosław Biedrowski <biedrowski.miroslaw@wp.pl>
2020-01-19 19:09:35 +0100

    Admin login admin news

--- routes/admin.js
+++ routes/admin.js
@@ -14,20 +14,40 @@ router.all("*", (req, res, next) => {
 
 /* GET news page. */
 router.get("/", (req, res) => {
-  // const newsData = new News({
-  //   title: "Tytuł testowy",
-  //   description: "Opis"
-  // });
-
-  // newsData.save(err => {
-  //   console.log(err);
-  // });
-
-  res.render("admin/index", { title: "Admin" });
+  News.find({}, (err, data) => {
+    console.log(data);
+    res.render("admin/index", { title: "Admin", data });
+  });
 });
 
 router.get("/news/add", (req, res) => {
-  res.render("admin/news-form", { title: "Dodaj artykuł" });
+  res.render("admin/news-form", {
+    title: "Dodaj artykuł",
+    body: {},
+    errors: {}
+  });
+});
+
+router.post("/news/add", (req, res) => {
+  const body = req.body;
+
+  const newsData = new News(body);
+  const errors = newsData.validateSync();
+
+  newsData.save(err => {
+    if (err) {
+      res.render("admin/news-form", { title: "Dodaj artykuł", errors, body });
+      return;
+    }
+
+    res.redirect("/admin");
+  });
+});
+
+router.get("/news/delete/:id", (req, res) => {
+  News.findByIdAndDelete(req.params.id, err => {
+    res.redirect("/admin");
+  });
 });
 
 module.exports = router;
